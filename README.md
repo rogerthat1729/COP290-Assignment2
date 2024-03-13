4c05e5f17b8670ef0902ccff191c2745bd425284
rogerthat1729 <cs1221603@iitd.ac.in>
2024-03-14 02:17:40 +0530

    pretty much final code - need to figure out size wise change of whisper model

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Experimentation:
 Audio - Python Speech recognition library, Whisper
 Video - Tested on base, base.en - 1 min 40 sec, small, small.en - 4 min, tiny.en - 1 min - Better results = more time - finalised base.en
-PDF - PyPDF2
+PDF - PyPDF2, pdfplumber, pymupdf
+issues with OCR - does not work for all images as pillow library constraints
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import whisper
-from moviepy.editor import VideoFileClip as VFC
-import PyPDF2
+import pytesseract as pt
+import io
+from PIL import Image
 import fitz as PyMuPDF
 import sys
 
@@ -8,6 +9,11 @@ type = sys.argv[1]
 name = sys.argv[2]
 file = open("output_txt_files/"+name[:-4]+".txt", "w")
 model = whisper.load_model("base.en")
+
+def img_to_txt(img):
+    image = Image.open(io.BytesIO(img))
+    txt = (pt.image_to_string(image, lang='eng')) + '\n'
+    return txt
 
 if type == 'video':
     path = "test_videos/"+name
@@ -27,6 +33,14 @@ elif type == 'pdf':
         for page_num in range(num_pages):
             text = pdfread[page_num].get_text()
             txt += ("Text on page:"+str(page_num)+"\n") + text + "\n"
+            for img in pdfread.get_page_images(page_num):
+                x = img[0]
+                base_img = pdfread.extract_image(x)
+                try:
+                    img_text = img_to_txt(base_img["image"])
+                    txt += img_text
+                except:
+                    print("Image format not supported on page " + str(page_num) + '\n')
     file.write(txt)
 else:
     print("Invalid input - give correct argument")
