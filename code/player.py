556852550b1bdf4c1bfc7f473ac685cf1b649e8b
rogerthat1729 <cs1221603@iitd.ac.in>
2024-04-03 17:40:12 +0530

    added movement animations

--- code/player.py
+++ code/player.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tasks import *
 import sys
+from support import import_folder
 
 all_tasks = ["Talk on phone", "Go to balcony", "Clean your room"]
 task_to_seq = {"Talk on phone": [pygame.K_p, pygame.K_h, pygame.K_o, pygame.K_n, pygame.K_e], "Go to balcony": [pygame.K_b, pygame.K_a, pygame.K_l, pygame.K_c, pygame.K_o, pygame.K_n, pygame.K_y], "Clean your room": [pygame.K_c, pygame.K_l, pygame.K_e, pygame.K_a, pygame.K_n]}
@@ -13,6 +14,11 @@ class Player(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-26)
 		self.events = []
+
+		self.import_player_assets()
+		self.status = 'down'
+		self.frame_index = 0
+		self.animation_speed = 0.15
 		
 		self.done_task = 0
 		self.wait = 0
@@ -26,20 +32,34 @@ class Player(pygame.sprite.Sprite):
 
 		self.obstacle_sprites = obstacle_sprites
 
+	def import_player_assets(self):
+		character_path = '../graphics/player/'
+		self.animations = {'up': [],'down': [],'left': [],'right': [],
+			'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
+			'right_attack':[],'left_attack':[],'up_attack':[],'down_attack':[]}
+
+		for animation in self.animations.keys():
+			full_path = character_path + animation
+			self.animations[animation] = import_folder(full_path)
+
 	def input(self):
 		keys = pygame.key.get_pressed()
 		if not self.is_textbox_active:
 			if keys[pygame.K_w]:
 				self.direction.y = -1
+				self.status = 'up'
 			elif keys[pygame.K_s]:
 				self.direction.y = 1
+				self.status = 'down'
 			else:
 				self.direction.y = 0
 
 			if keys[pygame.K_d]:
 				self.direction.x = 1
+				self.status = 'right'
 			elif keys[pygame.K_a]:
 				self.direction.x = -1
+				self.status = 'left'
 			else:
 				self.direction.x = 0
 			
@@ -63,6 +83,11 @@ class Player(pygame.sprite.Sprite):
 						char = event.unicode.upper()
 						if char.isalpha():
 							self.textbox_content += char
+	
+	def get_status(self):
+		if self.direction.x == 0 and self.direction.y == 0:
+			if not 'idle' in self.status:
+				self.status = self.status + '_idle'
 
 	def move(self,speed):
 		if self.direction.magnitude() != 0:
@@ -91,8 +116,23 @@ class Player(pygame.sprite.Sprite):
 						self.hitbox.bottom = sprite.hitbox.top
 					if self.direction.y < 0: # moving up
 						self.hitbox.top = sprite.hitbox.bottom
+	
+	def animate(self):
+		animation = self.animations[self.status]
+
+		# loop over the frame index 
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(animation):
+			self.frame_index = 0
+
+		# set the image
+		self.image = animation[int(self.frame_index)]
+		self.rect = self.image.get_rect(center = self.hitbox.center)
+
 
 	def update(self):
 		self.input()
+		self.get_status()
+		self.animate()
 		self.move(self.speed)
 		self.wait = max(0,self.wait-1)
