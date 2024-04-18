a085b981feb4853e9611b841c8c786b6cd025efd
rogerthat1729 <cs1221603@iitd.ac.in>
2024-04-18 22:55:36 +0530

    final commit (hopefully)

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 clean: run
 	@cd code; rm -rf __pycache__
 
-run:
+run: install
 	@cd code; python3 main.py;
+
+install:
+	@pip install -r requirements.txt
 	
--- code/player.py
+++ code/player.py
@@ -1,10 +1,8 @@
 import pygame 
 from settings import *
 from tasks import *
-# import sys
 from support import import_folder
 from settings import *
-# import time
 
 all_tasks = ["Talk on phone", "Go to balcony", "Clean your room"]
 task_to_seq = {"Talk on phone": [pygame.K_p, pygame.K_h, pygame.K_o, pygame.K_n, pygame.K_e], 
@@ -30,8 +28,6 @@ class Player(pygame.sprite.Sprite):
 		
 		self.done_task = 0
 		self.wait = 0
-		# self.is_textbox_active = False
-		# self.textbox_content = ""
 
 		self.popup = Popup("", 500, 60)
 
@@ -49,69 +45,6 @@ class Player(pygame.sprite.Sprite):
 		for animation in self.animations.keys():
 			full_path = character_path + animation
 			self.animations[animation] = import_folder(full_path)
-
-	# def input(self):
-		# if(self.nearest_obj):
-		# 	print("Object")
-		# else:
-		# 	print("No")
-
-		# keys = pygame.key.get_pressed()
-		# # if not self.is_textbox_active:
-		# if keys[pygame.K_w]:
-		# 	self.direction.y = -1
-		# 	self.status = 'up'
-		# elif keys[pygame.K_s]:
-		# 	self.direction.y = 1
-		# 	self.status = 'down'
-		# else:
-		# 	self.direction.y = 0
-
-		# if keys[pygame.K_d]:
-		# 	self.direction.x = 1
-		# 	self.status = 'right'
-		# elif keys[pygame.K_a]:
-		# 	self.direction.x = -1
-		# 	self.status = 'left'
-		# else:
-		# 	self.direction.x = 0
-		
-		# if keys[pygame.K_ESCAPE] and self.popup.active:
-		# 	self.popup.active = False
-			
-		# if keys[pygame.K_ESCAPE] and self.is_textbox_active:
-		# 	self.is_textbox_active = False
-
-		# if keys[pygame.K_1] and self.wait==0 and not self.popup.active:
-		# 	self.is_textbox_active = not self.is_textbox_active
-		# 	self.textbox_content = ""
-		# 	self.wait = 100
-
-		# for event in self.events:
-		# 	if event.type == pygame.QUIT:
-		# 		pygame.quit()
-		# 		sys.exit()
-			# elif event.type == pygame.KEYDOWN:
-				# if self.is_textbox_active:
-				# 	if event.key == pygame.K_BACKSPACE:
-				# 		self.textbox_content = self.textbox_content[:-1]
-				# 	else:
-				# 		char = event.unicode.upper()
-				# 		if char.isalpha():
-				# 			self.textbox_content += char
-		# 		if event.key == pygame.K_i:
-		# 			if not self.interact_time:
-		# 				self.interact_time = time.time()
-		# 	elif event.type == pygame.KEYUP:
-		# 		if event.key == pygame.K_i and self.interact_time:
-		# 			self.interact_time = None
-		
-		# if self.interact_time:
-		# 	if time.time() - self.interact_time >= 3:
-		# 		self.done_task = 1
-		# 		print("Task done")
-		# 		self.interact_time = None
-
 	
 	def get_status(self):
 		if self.direction.x == 0 and self.direction.y == 0:
@@ -149,18 +82,15 @@ class Player(pygame.sprite.Sprite):
 	def animate(self):
 		animation = self.animations[self.status]
 
-		# loop over the frame index 
 		self.frame_index += self.animation_speed
 		if self.frame_index >= len(animation):
 			self.frame_index = 0
 
-		# set the image
 		self.image = animation[int(self.frame_index)]
 		self.rect = self.image.get_rect(center = self.hitbox.center)
 
 
 	def update(self):
-		# self.input()
 		self.get_status()
 		self.animate()
 		self.move(self.speed)
